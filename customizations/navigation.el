19200b3247d65529bfb0a15319310202d5223d89
Tom Crinson <the.jaba@gmail.com>
2015-11-01 21:20:13 +0000

    Latest home

--- customizations/navigation.el
+++ customizations/navigation.el
@@ -59,3 +59,8 @@
 
 ;; projectile everywhere!
 (projectile-global-mode)
+
+(global-set-key (kbd "C-c <left>")  'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+(global-set-key (kbd "C-c <up>")    'windmove-up)
+(global-set-key (kbd "C-c <down>")  'windmove-down)
